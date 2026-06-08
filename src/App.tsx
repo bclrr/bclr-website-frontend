66bff2a8e748df00d32747ed58e7cd9c628a8565
Paul Bagot <bagotpa@gmail.com>
2026-06-08 21:03:51 +0200

    feat : add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
+export default function App() {
 const [count, setCount] = useState(0)
 
   return (
     <>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <div className="block p-4">
+          <h1 className="text-3xl font-bold underline">Test de tailwind</h1>
+        </div>
+        <div className="block  p-4">
+          <button className="bg-green-500" onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+        </div>
       </div>
     </>
   )
 }
 
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
+import './index.css'
+
+import App from './App.tsx'
 import Title from './components/Title.tsx'
 
 createRoot(document.getElementById('root')!).render(
